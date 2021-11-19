d977c005a1cda57c5fb1c9995451c17ff91f0627
limlesssj5781 <lishengjie94@yahoo.de>
2021-11-19 17:33:36 +0800

    Revert "ef"
    This reverts commit 25476847e879642adb48adfc35a669dbb0781b8b.

--- Cas9_predict_indel/src/utils/utils.py
+++
@@ -1,8 +0,0 @@
-#
-def load_data(file):
-    data = []
-    with open(file,"r") as f:
-        for line in f:
-            line = line.split()
-            data.append((line[0], float(line[1])))
-    return data
